3f360a7bb6490c3e1cb1e5c7a772dc87951f0b61
Shivam Dhuria <abc@Shivams-MacBook-Pro.local>
2021-11-20 22:52:04 +0530

    first commit

---
+++ app/src/main/java/com/elixer/surface/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.elixer.surface
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.elixer.surface.ui.theme.SurfaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SurfaceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun CircleShape(){
+    ExampleBox(shape = CircleShape)
+}
+
+@Composable
+fun ExampleBox(shape: Shape){
+    Column(modifier = Modifier.fillMaxWidth().wrapContentSize(Alignment.Center)) {
+        Box(
+            modifier = Modifier.size(300.dp).clip(shape).background(Color.Red)
+        )
+    }
+}
+
+@Preview(showBackground = true,showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    SurfaceTheme {
+        Greeting("Android")
+        CircleShape()
+    }
+}
